e2e9754120bc336e073cc65c847bc3d8cad8b620
Aleksei Kuzmin <alkuzmin@microsoft.com>
2017-11-23 14:02:43 +0100

    Move IDMap to base namespace.
    https://chromium-review.googlesource.com/618801

--- atom/browser/atom_permission_manager.h
+++ atom/browser/atom_permission_manager.h
@@ -9,7 +9,7 @@
 #include <vector>
 
 #include "base/callback.h"
-#include "base/id_map.h"
+#include "base/containers/id_map.h"
 #include "content/public/browser/permission_manager.h"
 
 namespace content {
@@ -76,7 +76,7 @@ class AtomPermissionManager : public content::PermissionManager {
 
  private:
   class PendingRequest;
-  using PendingRequestsMap = IDMap<std::unique_ptr<PendingRequest>>;
+  using PendingRequestsMap = base::IDMap<std::unique_ptr<PendingRequest>>;
 
   RequestHandler request_handler_;
 
