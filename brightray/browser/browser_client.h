3d228eef2f54b32712b696461538b90a5b4a9b74
deepak1556 <hop2deep@gmail.com>
2017-11-29 22:30:21 +0530

    FIX: add chrome-devtools:// to list of WebUI schemes https://chromium-review.googlesource.com/c/chromium/src/+/617598

--- brightray/browser/browser_client.h
+++ brightray/browser/browser_client.h
@@ -44,6 +44,8 @@ class BrowserClient : public content::ContentBrowserClient {
       override;
   void GetAdditionalAllowedSchemesForFileSystem(
       std::vector<std::string>* additional_schemes) override;
+  void GetAdditionalWebUISchemes(
+      std::vector<std::string>* additional_schemes) override;
   net::NetLog* GetNetLog() override;
   base::FilePath GetDefaultDownloadDirectory() override;
   content::DevToolsManagerDelegate* GetDevToolsManagerDelegate() override;
